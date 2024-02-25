005bf7f060278b424e6ce1b4177fa28c36376385
Pavel Kalian <pavel@kalian.cz>
2024-02-25 18:45:37 -0300

    Add file with las run timestamp

--- scripts/openskiron.sh
+++ scripts/openskiron.sh
@@ -6,4 +6,4 @@ $(dirname $(realpath $0))/getalllinks.py https://openskiron.org/gribs_icon-d2/ I
 $(dirname $(realpath $0))/getalllinks.py https://openskiron.org/saildocs/ WRF_MINIMAL
 $(dirname $(realpath $0))/getalllinks.py https://openskiron.org/gribs_wrf_4km/ 4km_WRF_WAM
 $(dirname $(realpath $0))/getalllinks.py https://openskiron.org/gribs_wrf_12km/ 12km_WRF_WAM
-$(dirname $(realpath $0))/getalllinks.py https://openskiron.org/gribs_skiron/ SKIRON 
+$(dirname $(realpath $0))/getalllinks.py https://openskiron.org/gribs_skiron/ SKIRON
